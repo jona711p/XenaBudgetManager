9d439de2470900dc8f7651fdaede86ea23ff47c4
Jonas Lykke <jona711p@student.eadania.dk>
2017-12-01 15:32:14 +0100

    Rettet Type-O's :)

--- XenaBudgetManager/Models/Xena.cs
+++ XenaBudgetManager/Models/Xena.cs
@@ -7,6 +7,7 @@ namespace XenaBudgetManager.Models
     public class Xena
     {
         public string access_token { get; set; }
+
         public string id_code { get; set; }
 
         /// <summary>
