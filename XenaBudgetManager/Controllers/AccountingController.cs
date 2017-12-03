7b333eb0c90529d50527b48531fa5593c16f1008
Mikael David Paaske <mika2466@student.eadania.dk>
2017-12-03 12:01:18 +0100

    View start

---
+++ XenaBudgetManager/Controllers/AccountingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XenaBudgetManager.Models;
+
+namespace XenaBudgetManager.Controllers
+{
+    public class AccountingController : Controller
+    {
+        [HttpGet]
+        public ActionResult Accounting(XenaDataModel data)
+        {
+            return View();
+        }
+
+    }
+}
--- XenaBudgetManager/Models/XenaDataModel.cs
+++ XenaBudgetManager/Models/XenaDataModel.cs
@@ -7,5 +7,36 @@ namespace XenaBudgetManager.Models
 {
     public class XenaDataModel
     {
+        // Months
+        public string January { get; set; }
+        public string February { get; set; }
+        public string March { get; set; }
+        public string April { get; set; }
+        public string May { get; set; }
+        public string June { get; set; }
+        public string July  { get; set; }
+        public string August { get; set; }
+        public string September { get; set; }
+        public string October { get; set; }
+        public string November { get; set; }
+        public string December { get; set; }
+
+        // Finance Groups
+        public string Revenue { get; set; }
+        public string ProductConsumption { get; set; }
+        public string StaffCost { get; set; }
+        public string LocalCost { get; set; }
+        public string TheResultOfTheYear { get; set; }
+
+
+
+        // finance Accounts
+        public string Revenue_DogFood { get; set; }
+        public string Revenue_CatFood { get; set; }
+        public string ProductConsumption_DogFood { get; set; }
+        public string ProductConsumption_CatFood { get; set; }
+        public string StaffCost_Pay { get; set; }
+        public string LocalCost_Rent { get; set; }
+        public string LocalCost_Light_Heat { get; set; }
     }
 }
