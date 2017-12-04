ff4a6a0ae64379006a57136c9dd909cf91791d5e
Thomas Nielsen <mini_hippo@hotmail.com>
2017-12-04 12:50:43 +0100

    Tilføjede klasser lavet udfra diagrammet

---
+++ XenaBudgetManager/Models/AccountGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace XenaBudgetManager.Models
+{
+    public class AccountGroup
+    {
+        [DisplayName("Kontogruppe ID")]
+        public int accountGroupID { get; set; }
+
+        [DisplayName("Kontogruppenavn")]
+        public string accountGroupName { get; set; }
+
+        [DisplayName("Kontoliste")]
+        public List<Account> accountList { get; set; }
+    }
+}
---
+++ XenaBudgetManager/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace XenaBudgetManager.Models
+{
+    public class User
+    {
+        [DisplayName("User ID")]
+        public int userID { get; set; }
+    }
+}
